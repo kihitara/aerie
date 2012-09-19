f0692d34294923e425e7bedc7434bfb90629862d
Miriam <kihitara@gmail.com>
2012-09-19 15:03:57 +1200

    Despite changing the IDs, blocks still don't work

--- layout/general.php
+++ layout/general.php
@@ -5,8 +5,8 @@ $hasnavbar = (empty($PAGE->layout_options['nonavbar']) && $PAGE->has_navbar());
 $hasfooter = (empty($PAGE->layout_options['nofooter']));
 $hassidepre = (empty($PAGE->layout_options['noblocks']) && $PAGE->blocks->region_has_content('side-pre', $OUTPUT));
 $hassidepost = (empty($PAGE->layout_options['noblocks']) && $PAGE->blocks->region_has_content('side-post', $OUTPUT));
-$hastopblocks = (empty($PAGE->layout_options['noblocks']) && $PAGE->blocks->region_has_content('block-top-left', $OUTPUT) or $PAGE->blocks->region_has_content('block-top-mid', $OUTPUT) or  $PAGE->blocks->region_has_content('block-top-right', $OUTPUT));
-$hasbtmblocks = (empty($PAGE->layout_options['noblocks']) && $PAGE->blocks->region_has_content('block-btm-left', $OUTPUT) or $PAGE->blocks->region_has_content('block-btm-mid', $OUTPUT) or  $PAGE->blocks->region_has_content('block-btm-right', $OUTPUT));
+$hastopblocks = (empty($PAGE->layout_options['noblocks']) && $PAGE->blocks->region_has_content('blocktopleft', $OUTPUT) or $PAGE->blocks->region_has_content('blocktopmid', $OUTPUT) or  $PAGE->blocks->region_has_content('blocktopright', $OUTPUT));
+$hasbtmblocks = (empty($PAGE->layout_options['noblocks']) && $PAGE->blocks->region_has_content('blockbtmleft', $OUTPUT) or $PAGE->blocks->region_has_content('blockbtmmid', $OUTPUT) or  $PAGE->blocks->region_has_content('blockbtmright', $OUTPUT));
 
 $showsidepre = ($hassidepre && !$PAGE->blocks->region_completely_docked('side-pre', $OUTPUT));
 $showsidepost = ($hassidepost && !$PAGE->blocks->region_completely_docked('side-post', $OUTPUT));
@@ -94,21 +94,21 @@ echo $OUTPUT->doctype() ?>
 		    <div class="top-blocks">
 		        <div id="blocktopleft" class="block-region">
 		            <div class="region-content">
-                        <?php echo $OUTPUT->blocks_for_region('block-top-left') ?>
+                        <?php echo $OUTPUT->blocks_for_region('blocktopleft') ?>
                     </div>
 		        </div>
 		    </div>
 		    <div class="top-blocks">
 		        <div id="blocktopmid" class="block-region">
 		            <div class="region-content">
-                        <?php echo $OUTPUT->blocks_for_region('block-top-mid') ?>
+                        <?php echo $OUTPUT->blocks_for_region('blocktopmid') ?>
                     </div>
 		        </div>
 		    </div>
 		    <div class="top-blocks">
 		        <div id="blocktopright" class="block-region">
 		            <div class="region-content">
-                        <?php echo $OUTPUT->blocks_for_region('block-top-right') ?>
+                        <?php echo $OUTPUT->blocks_for_region('blocktopright') ?>
                     </div>
 		        </div>
 		    </div>
@@ -160,21 +160,21 @@ echo $OUTPUT->doctype() ?>
 		    <div class="btm-blocks">
 		        <div id="blockbtmleft" class="block-region">
 		            <div class="region-content">
-                        <?php echo $OUTPUT->blocks_for_region('block-btm-left') ?>
+                        <?php echo $OUTPUT->blocks_for_region('blockbtmleft') ?>
                     </div>
 		        </div>
 		    </div>
 		    <div class="btm-blocks">
 		        <div id="blockbtmmid" class="block-region">
 		            <div class="region-content">
-                        <?php echo $OUTPUT->blocks_for_region('block-btm-mid') ?>
+                        <?php echo $OUTPUT->blocks_for_region('blockbtmmid') ?>
                     </div>
 		        </div>
 		    </div>
 		    <div class="btm-blocks">
 		        <div id="blockbtmright" class="block-region">
 		            <div class="region-content">
-                        <?php echo $OUTPUT->blocks_for_region('block-btm-right') ?>
+                        <?php echo $OUTPUT->blocks_for_region('blockbtmright') ?>
                     </div>
 		        </div>
 		    </div>
